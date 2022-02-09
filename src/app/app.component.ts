110950fb26318eefce2bdd4ceb1323073f887f00
adimerpaul <adimer101@email.com>
2022-02-08 20:47:09 -0400

    terminado las variables con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pruebas';
+  contador=1
+  nombre ="adimer paul"
+  apellido ="chambi ajata"
+  edad = 10
+  email="adimer101@gmail.com"
+  sueldos=[7000,8000,10000]
+  activo=true
+  esActivo(){
+    if(this.activo){
+      return 'Trabajador activo'
+    }else{
+      return 'Trabajador inactivo'
+    }
+  }
+  ultimossueldos(){
+    let suma=0
+    for(let i=0;i<this.sueldos.length;i++){
+      suma=suma+this.sueldos[i]
+    }
+    return suma
+  }
+  sumar(){
+    this.contador++
+  }
+  restar(){
+    if(this.contador>0){
+      this.contador--
+    }
+
+  }
+
 }
